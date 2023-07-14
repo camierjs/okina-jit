a661fce335487a548f0e56cffacb89bbdd4d8e9f
camierjs <camier1@llnl.gov>
2023-07-13 17:49:36 -0700

    Try hard coded path

--- config/defaults.mk
+++ config/defaults.mk
@@ -222,7 +222,7 @@ HYPRE_DIR = @MFEM_DIR@/../hypre/src/hypre
 HYPRE_OPT = -I$(HYPRE_DIR)/include
 HYPRE_LIB = -L$(HYPRE_DIR)/lib -lHYPRE
 ifeq (YES,$(SHARED))
-	HYPRE_LIB += $(XLINKER)-rpath,$(HYPRE_DIR)/lib
+	HYPRE_LIB += $(XLINKER)-rpath,$(realpath $(HYPRE_DIR))/lib
 endif
 ifeq (YES,$(MFEM_USE_CUDA))
    # This is only necessary when hypre is built with cuda:
@@ -246,7 +246,7 @@ ifeq ($(MFEM_USE_SUPERLU)$(MFEM_USE_STRUMPACK)$(MFEM_USE_MUMPS),NONONO)
      METIS_LIB = -L$(METIS_DIR)/lib -lmetis
    endif
 	ifeq (YES,$(SHARED))
-		METIS_LIB += $(XLINKER)-rpath,$(METIS_DIR)/lib
+		METIS_LIB += $(XLINKER)-rpath,$(realpath $(METIS_DIR))/lib
 	endif
 else
    # ParMETIS: currently needed by SuperLU or STRUMPACK. We assume that METIS 5
@@ -258,7 +258,7 @@ else
    METIS_LIB = -L$(METIS_DIR)/lib -lparmetis -lmetis
    MFEM_USE_METIS_5 = YES
 	ifeq (YES,$(SHARED))
-		METIS_LIB += $(XLINKER)-rpath,$(METIS_DIR)/lib
+		METIS_LIB += $(XLINKER)-rpath,$(realpath $(METIS_DIR))/lib
 	endif
 endif
 
